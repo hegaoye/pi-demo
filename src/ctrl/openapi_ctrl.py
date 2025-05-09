b86a62a152b91d1475158df0c0110a341c2ef8d9
Tesla <Tesla@gmail.com>
2025-05-10 00:04:44 +0800

    init

--- src/ctrl/openapi_ctrl.py
+++ src/ctrl/openapi_ctrl.py
@@ -1,7 +1,17 @@
 from flask import Blueprint
 
+from src.driver.switch_driver import SwitchDriver
+
 openapi = Blueprint('openapi', __name__)
 
-@openapi.route("/")
-def hello():
-    return "Hello World!"
+
+@openapi.route("/switch/on")
+def on():
+    SwitchDriver(25).on()
+    return ("pin 25 on")
+
+
+@openapi.route("/switch/off")
+def off():
+    SwitchDriver(25).off()
+    return "pin 25 off"
