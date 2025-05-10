edce671c5c9235045736f9a208aab4dfa63cd146
Tesla <Tesla@gmail.com>
2025-05-10 14:57:35 +0800

    init

--- src/ctrl/openapi_ctrl.py
+++ src/ctrl/openapi_ctrl.py
@@ -8,10 +8,10 @@ openapi = Blueprint('openapi', __name__)
 @openapi.route("/relay/on", methods=['GET'])
 def on():
     RelayDriver(25).on()
-    return jsonify({"code": "0000", "success": False, "info": "on"})
+    return jsonify({"code": "0000", "info": "on"})
 
 
 @openapi.route("/relay/off", methods=['GET'])
 def off():
     RelayDriver(25).off()
-    return jsonify({"code": "0000", "success": False, "info": "off"})
+    return jsonify({"code": "0000", "info": "off"})
