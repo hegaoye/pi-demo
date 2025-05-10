75c385e3bab56c78b852510e910c045f708d7dca
Tesla <Tesla@gmail.com>
2025-05-10 18:51:59 +0800

    init

--- src/ctrl/openapi_ctrl.py
+++ src/ctrl/openapi_ctrl.py
@@ -1,17 +1,18 @@
 from flask import Blueprint, jsonify
 
+from src.config.status_enum import Status
 from src.driver.relay_driver import RelayDriver
 
 openapi = Blueprint('openapi', __name__)
 
 
-@openapi.route("/relay/on", methods=['GET'])
-def on():
-    RelayDriver(25).on()
-    return jsonify({"code": "0000", "info": "on"})
+@openapi.route("/relay/<onoff>", methods=['GET'])
+def on(onoff):
+    if onoff == str(Status.On).lower():
+        RelayDriver(25).on()
+        info = "on"
+    elif onoff == str(Status.Off).lower():
+        RelayDriver(25).off()
+        info = "off"
 
-
-@openapi.route("/relay/off", methods=['GET'])
-def off():
-    RelayDriver(25).off()
-    return jsonify({"code": "0000", "info": "off"})
+    return jsonify({"code": "0000", "info": info})
