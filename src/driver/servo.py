3431a93c9b8996011566ec3f9a308b226c610a57
Tesla <Tesla@gmail.com>
2025-05-14 19:33:31 +0800

    init

---
+++ src/driver/servo.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+
+from time import sleep
+
+ctrlpin = 12  # 指定引脚，物理引脚编号32
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)  # 设置引脚编号系统，BOARD指的是物理引脚命名方式
+GPIO.setup(ctrlpin, GPIO.OUT)  # 设置引脚32为输出
+
+gs90_pwm = GPIO.PWM(ctrlpin, 50)  # 创建gs90_pwm实例，并设置频率为50Hz
+gs90_pwm.start(0)  # 启动gs90_pwm实例，占空比为0
+
+
+# 根据输入参数类型修改占空比
+def gs90_angle(angle):
+    if isinstance(angle, str):
+        if angle.upper() == 'STOP':
+            gs90_pwm.ChangeDutyCycle(0)
+        else:
+            print('Input invalid')
+    elif isinstance(angle, int) or isinstance(angle, float):
+        gs90_pwm.ChangeDutyCycle(2.5 + angle * 10 / 180)
+
+
+if __name__ == "__main__":
+
+    while True:
+
+        for angle in range(0, 270, 45):
+            gs90_angle(angle)
+            sleep(0.1)
+            gs90_angle('stop')
+            sleep(0.5)
+
+        for angle in range(270, 0, -45):
+            gs90_angle(angle)
+            sleep(0.1)
+            gs90_angle('stop')
+            sleep(0.5)
--- src/driver/servo_motor_driver.py
+++ src/driver/servo_motor_driver.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import atexit
 import time
+
 import RPi.GPIO as GPIO
 
 
@@ -11,23 +12,29 @@ class ServoMotorDriver(object):
 
     def __init__(self):
         atexit.register(GPIO.cleanup)
+        GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(14, GPIO.OUT, initial=False)
-        self.p = GPIO.PWM(14, 50)
+        GPIO.setup(13, GPIO.OUT)
+        self.p = GPIO.PWM(13, 50)
 
     def openLock(self):
         """
         开锁控制
+        0.5	-90
+        1.0	-45
+        1.5	0
+        2.0	45
+        2.5	90
         """
-
         self.p.start(0)
         time.sleep(0.02)
         self.p.ChangeDutyCycle(2)
         time.sleep(1)
         self.p.ChangeDutyCycle(0)
-        time.sleep(0.2)
-        time.sleep(1)
-        self.lock()
+        atexit.register(GPIO.cleanup)
+        # time.sleep(0.2)
+        # time.sleep(1)
+        # self.lock()
 
     def lock(self):
         """
@@ -40,7 +47,30 @@ class ServoMotorDriver(object):
         self.p.ChangeDutyCycle(0)
         time.sleep(0.2)
 
+    # 根据输入参数类型修改占空比
+    def gs90_angle(self,angle):
+
+        if isinstance(angle, str):
+            if angle.upper() == 'STOP':
+                self.p.ChangeDutyCycle(0)
+            else:
+                print('Input invalid')
+        elif isinstance(angle, int) or isinstance(angle, float):
+            self.p.ChangeDutyCycle(2.5 + angle * 10 / 180)
+
 
 if __name__ == "__main__":
     lockSV = ServoMotorDriver()
-    lockSV.openLock()
+
+    while True:
+        for angle in range(0, 180, 45):
+            lockSV.gs90_angle(angle)
+            time.sleep(0.1)
+            lockSV.gs90_angle('stop')
+            time.sleep(0.5)
+
+        for angle in range(180, 0, -45):
+            lockSV.gs90_angle(angle)
+            time.sleep(0.1)
+            lockSV.gs90_angle('stop')
+            time.sleep(0.5)
