951467e24bc7273493ced11b0f23e88e89b1a245
Tesla <Tesla@gmail.com>
2025-05-25 14:48:36 +0800

    init

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from flasgger import Swagger
 from flask import Flask
 from flask_restful import Api
 
+from src.resource.button_resource import ButtonResource
 from src.resource.relay_resource import RelayResource
 from src.resource.servo_resource import ServoResource
 
@@ -16,6 +17,7 @@ Swagger(app)
 api = Api(app)
 api.add_resource(ServoResource, '/servo/<int:gpio>/<int:angle>/<int:total_angle>', endpoint='servo')
 api.add_resource(RelayResource, '/relay/<int:gpio>/<string:onoff>', endpoint='relay')
+api.add_resource(ButtonResource, '/button/<int:gpio>', endpoint='button')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ src/resource/button_resource.py
@@ -0,0 +1,40 @@
+from flask_restful import Resource
+
+
+class ButtonResource(Resource):
+    """
+    机械开关控制
+    """
+
+    def get(self, callback, gpio=26):
+        """
+        机械开关控制 API
+        ---
+        tags:
+          - 继电器开关控制 API
+        parameters:
+          - name: gpio
+            in: path
+            type:  integer
+            required: true
+            description: 板子上 gpio 编号 如：G12,G13,G17,G18...
+          - name: callback
+            in: parameter
+            type: string
+            required: true
+            description: 回调通知地址
+        responses:
+          500:
+            description: 指令错误
+          200:
+            description: 指令执行成功
+            schema:
+              id: R
+              properties:
+                code:
+                  type: string
+                  description: 状态码
+                  default: "0000"
+        """
+
+        return {"code": "0000", "info": callback}
