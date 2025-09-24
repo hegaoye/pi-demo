4bc7426c5f6b8ad35a4abaf78950084a6a2215fa
Tesla <Tesla@gmail.com>
2025-09-24 21:59:39 +0800

    增加配置文件

--- src/driver/l298n_driver.py
+++ src/driver/l298n_driver.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from time import sleep
 
 import RPi.GPIO as GPIO
 
@@ -22,79 +23,106 @@ class L298NMotorDriver(object):
         GPIO.setup(in3_pin, GPIO.OUT)  # 设置引脚
         GPIO.setup(in4_pin, GPIO.OUT)  # 设置引脚
 
-        self.ena_pin = GPIO.PWM(ena_pin, 50)  # 创建电机pwm实例，并设置频率为50Hz
-        self.enb_pin = GPIO.PWM(enb_pin, 50)  # 创建电机pwm实例，并设置频率为50Hz
-        # 默认设置0频率
-        self.ena_pin.ChangeDutyCycle(0)
-        self.enb_pin.ChangeDutyCycle(0)
+        self.ena_pin = GPIO.PWM(ena_pin, 100)  # 创建电机pwm实例，并设置频率为100Hz
+        self.enb_pin = GPIO.PWM(enb_pin, 100)  # 创建电机pwm实例，并设置频率为100Hz
 
-        self.in1_pin = GPIO.output(in1_pin, GPIO.LOW)  # 创建电机pwm实例，并设置频率为50Hz
-        self.in2_pin = GPIO.output(in2_pin, GPIO.LOW)  # 创建电机pwm实例，并设置频率为50Hz
-
-        self.in3_pin = GPIO.output(in3_pin, GPIO.LOW)  # 创建电机pwm实例，并设置频率为50Hz
-        self.in4_pin = GPIO.output(in4_pin, GPIO.LOW)  # 创建电机pwm实例，并设置频率为50Hz
+        self.in1_pin = in1_pin
+        self.in2_pin = in2_pin
+        self.in3_pin = in3_pin
+        self.in4_pin = in4_pin
 
     def start(self):
         """
         开始
         """
         # 默认设置0频率
-        self.ena_pin.ChangeDutyCycle(0)
-        self.enb_pin.ChangeDutyCycle(0)
-        self.in1_pin.start(0)
-        self.in2_pin.start(0)
-        self.in3_pin.start(0)
-        self.in4_pin.start(0)
+        self.ena_pin.start(0)
+        self.enb_pin.start(0)
+        GPIO.output(self.in1_pin, GPIO.LOW)
+        GPIO.output(self.in2_pin, GPIO.LOW)
+        GPIO.output(self.in3_pin, GPIO.LOW)
+        GPIO.output(self.in4_pin, GPIO.LOW)
 
     def stop(self):
         """
         停止
         """
-        self.in1_pin.stop()
-        self.in2_pin.stop()
-        self.in3_pin.stop()
-        self.in4_pin.stop()
+        self.ena_pin.stop()
+        self.enb_pin.stop()
+        GPIO.output(self.in1_pin, False)
+        GPIO.output(self.in2_pin, False)
+        GPIO.output(self.in3_pin, False)
+        GPIO.output(self.in4_pin, False)
 
     def forward(self, pwm):
         """
         正转，pwm 控制速度
         """
         # ENA
-        self.in1_pin.ChangeDutyCycle(pwm)
-        self.in2_pin.ChangeDutyCycle(0)
+        self.ena_pin.ChangeDutyCycle(pwm)
+        GPIO.output(self.in1_pin, GPIO.HIGH)
+        GPIO.output(self.in2_pin, GPIO.LOW)
         # ENB
-        self.in3_pin.ChangeDutyCycle(pwm)
-        self.in4_pin.ChangeDutyCycle(0)
+        self.enb_pin.ChangeDutyCycle(pwm)
+        GPIO.output(self.in3_pin, GPIO.HIGH)
+        GPIO.output(self.in4_pin, GPIO.LOW)
 
     def reverse(self, pwm):
         """
         反转，pwm 控制速度
         """
         # ENA
-        self.in1_pin.ChangeDutyCycle(0)
-        self.in2_pin.ChangeDutyCycle(pwm)
+        self.ena_pin.ChangeDutyCycle(pwm)
+        GPIO.output(self.in1_pin, GPIO.LOW)
+        GPIO.output(self.in2_pin, GPIO.HIGH)
         # ENB
-        self.in3_pin.ChangeDutyCycle(0)
-        self.in4_pin.ChangeDutyCycle(pwm)
+        self.enb_pin.ChangeDutyCycle(pwm)
+        GPIO.output(self.in3_pin, GPIO.LOW)
+        GPIO.output(self.in4_pin, GPIO.HIGH)
 
     def turn_left(self, pwm):
         """
         左转
         """
         # ENA
-        self.in1_pin.ChangeDutyCycle(0)
-        self.in2_pin.ChangeDutyCycle(0)
+        self.ena_pin.ChangeDutyCycle(pwm)
+        GPIO.output(self.in1_pin, GPIO.LOW)
+        GPIO.output(self.in2_pin, GPIO.HIGH)
         # ENB
-        self.in3_pin.ChangeDutyCycle(pwm)
-        self.in4_pin.ChangeDutyCycle(0)
+        self.enb_pin.ChangeDutyCycle(pwm)
+        GPIO.output(self.in3_pin, GPIO.HIGH)
+        GPIO.output(self.in4_pin, GPIO.LOW)
 
     def turn_right(self, pwm):
         """
         右转
         """
         # ENA
-        self.in1_pin.ChangeDutyCycle(pwm)
-        self.in2_pin.ChangeDutyCycle(0)
+        self.ena_pin.ChangeDutyCycle(pwm)
+        GPIO.output(self.in1_pin, GPIO.HIGH)
+        GPIO.output(self.in2_pin, GPIO.LOW)
         # ENB
-        self.in3_pin.ChangeDutyCycle(0)
-        self.in4_pin.ChangeDutyCycle(0)
+        self.enb_pin.ChangeDutyCycle(pwm)
+        GPIO.output(self.in3_pin, GPIO.LOW)
+        GPIO.output(self.in4_pin, GPIO.HIGH)
+
+
+if __name__ == '__main__':
+    ENA = 15
+    IN1 = 4
+    IN2 = 14
+
+    ENB = 24
+    IN3 = 22
+    IN4 = 23
+    l298n_motor = L298NMotorDriver(ENA, IN1, IN2, ENB, IN3, IN4)
+    l298n_motor.start()
+    l298n_motor.forward(100)
+    sleep(10)
+    l298n_motor.reverse(100)
+    sleep(10)
+    l298n_motor.turn_left(100)
+    sleep(10)
+    l298n_motor.turn_right(100)
+    sleep(10)
+    l298n_motor.stop()
